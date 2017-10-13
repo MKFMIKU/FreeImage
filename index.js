40a9bf43f5bdf84d590c9dd781e2ee85f05cee89
meikangfu <meikangfu@jd.com>
2017-10-13 18:05:58 +0800

    fix directory bug

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = function(width, height, cb) {
     let name = 'Free_' + width + 'x' + height + '.png';
     let tag = width + ' x ' + height;
     let fontSize = 32;
-    Jimp.loadFont( './font/open-sans-32-black.fnt' ).then(function (font) { // load font from .fnt file
+    Jimp.loadFont( __dirname + '/font/open-sans-32-black.fnt' ).then(function (font) { // load font from .fnt file
         image.print(font, width/2-tag.length*fontSize/4, height/2-fontSize/4, tag);
         image.write(name);
         cb(name);
