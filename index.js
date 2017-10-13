86cc69ef045afe74cd95987a09ed1f6a36b6b32a
meikangfu <meikangfu@jd.com>
2017-10-13 17:39:58 +0800

    complete termnial use

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-var Buffer = require('buffer').Buffer,
-    Jpeg = require('jpeg').Jpeg;
-
-function randomColorComponent() {
-    return Math.floor(Math.random() * 256);
-}
+const Jimp = require("jimp");
 
 /**
  * Creates a random image
  */
-module.exports = function(width, height, callback) {
-    var buffer = new Buffer(width * height * 3);
-
-    for (var x = 0; x < width; x++) {
-        for (var y = 0; y < height; y++) {
-            var pixelStart = x * width * 3 + y * 3;
-
-            buffer[pixelStart + 0] = randomColorComponent();
-            buffer[pixelStart + 1] = randomColorComponent();
-            buffer[pixelStart + 2] = randomColorComponent();
-        }
-    }
-
-    var image = new Jpeg(buffer, width, height);
-
-    image.encode(function(result) {
-        callback(null, result);
+module.exports = function(width, height, cb) {
+    let image = new Jimp(width, height, 0xffffffff);
+    let name = 'Free_' + width + 'x' + height + '.png';
+    let tag = width + ' x ' + height;
+    let fontSize = 32;
+    Jimp.loadFont( './font/open-sans-32-black.fnt' ).then(function (font) { // load font from .fnt file
+        image.print(font, width/2-tag.length*fontSize/4, height/2-fontSize/4, tag);
+        image.write(name);
+        cb(name);
     });
 };
